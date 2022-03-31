69acd800c7f9b9ae06b173e3af696a184ca05823
Anton Kupranis <anton.kupranis@cndt.app>
2022-03-31 15:31:12 +0300

    v2 without custom task

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
 from functools import wraps
+from contextlib import contextmanager
 
-from celery import Celery, Task
+from celery import Celery
 from redis import StrictRedis
 
 BROKER_URL = 'redis://localhost:6379/0'
@@ -12,39 +13,49 @@ RETRY_SEC = 8
 celery = Celery("tasks", broker=BROKER_URL, backend=BACKEND_URL)
 redis_client = StrictRedis(host="localhost", port=6379, decode_responses=True)
 
-
-class LimitedTask(Task):
-
-    def __init__(self, *args, **kwargs):
-        self.redis = redis_client
-        super().__init__(*args, **kwargs)
-
-    def on_success(self, retval, task_id, args, kwargs):
-        self._release_key(args[0])
-
-    def on_failure(self, exc, task_id, args, kwargs, einfo):
-        self._release_key(args[0])
-
-    def _release_key(self, task_key):
-        r = self.redis.decr(task_key, 1)
+_LUA_SHA = None
+_LUA_SCRIPT = '''
+    local e = redis.call('GET', KEYS[1])
+    if not e then
+        redis.call('SET', KEYS[1], 1, 'EX', ARGV[2])
+        return 1
+    elseif e < ARGV[1] then
+        redis.call('SET', KEYS[1], e + 1, 'EX', ARGV[2])
+        return e + 1
+    end
+    return 0   
+'''
 
 
-def decor(func):
+@contextmanager
+def key_release(key):
+    try:
+        yield
+    finally:
+        redis_client.decr(key, 1)
+
+
+def task_id_limiter(func):
     @wraps(func)
     def inner(self, *args, **kwargs):
         key = args[0]
-        check_key = int(self.redis.incr(key, 1))
-        if check_key <= KEY_LIMIT:
-            return func(self, *args, **kwargs)
+        global _LUA_SHA
+        if _LUA_SHA is None:
+            _LUA_SHA = redis_client.register_script(_LUA_SCRIPT)
+
+        check_key = _LUA_SHA(keys=[key], args=[KEY_LIMIT, 60])
+        if check_key:
+            with key_release(key):
+                return func(self, *args, **kwargs)
         else:
             self._release_key(key)
             self.retry(countdown=RETRY_SEC)
     return inner
 
 
-@celery.task(bind=True, base=LimitedTask, max_retries=200)
-@decor
-def dummy_task(self, task_key, sec_to_sleep):
+@celery.task
+@task_id_limiter
+def dummy_task(task_key, sec_to_sleep):
     time.sleep(sec_to_sleep)
     return sec_to_sleep
 
