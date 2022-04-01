55de3ce84ad97db27b47895dacb78839f78a8e12
Anton Kupranis <anton.kupranis@cndt.app>
2022-04-01 11:41:38 +0300

    upd lua scripts and retry func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import inspect
 from functools import wraps
 from contextlib import contextmanager
 
@@ -13,62 +14,76 @@ RETRY_SEC = 8
 celery = Celery("tasks", broker=BROKER_URL, backend=BACKEND_URL)
 redis_client = StrictRedis(host="localhost", port=6379, decode_responses=True)
 
-_LUA_SHA = None
-_LUA_SCRIPT = '''
-    local e = redis.call('GET', KEYS[1])
-    if not e then
-        redis.call('SET', KEYS[1], 1, 'EX', ARGV[2])
-        return 1
-    elseif e < ARGV[1] then
-        redis.call('SET', KEYS[1], e + 1, 'EX', ARGV[2])
-        return e + 1
-    end
-    return 0   
+_LUA_SHA_INCREASE = None
+_LUA_SCRIPT_INCREASE = '''
+    local e = redis.call('GET', KEYS[1]) or 0
+    e = tonumber(e) + 1
+    if e <= tonumber(ARGV[1]) then redis.call('SET', KEYS[1], e, 'EX', tonumber(ARGV[2])) end
+    return e
 '''
-_LUA_SCRIPT_ = '''
-    local e = redis.call('GET', KEYS[1])
-    if not e or e < 1 then
-        redis.call('SET', KEYS[1], 0, 'EX', ARGV[2])
-        return 0
-    else
-        redis.call('SET', KEYS[1], e - 1, 'EX', ARGV[2])
-        return e
-    end
+_LUA_SHA_DECREASE = None
+_LUA_SCRIPT_DECREASE = '''
+    local e = redis.call('GET', KEYS[1]) or 0
+    e = tonumber(e)
+    if e < 1 then e = 1 end
+    e = e - 1
+    redis.call('SET', KEYS[1], e, 'EX', tonumber(ARGV[1]))
+    return e
 '''
 
 
 @contextmanager
-def key_release(key):
+def key_releaser(key):
     try:
         yield
     finally:
-        global _LUA_SHA
-        if _LUA_SHA is None:
-            _LUA_SHA = redis_client.register_script(_LUA_SCRIPT_)
+        global _LUA_SHA_DECREASE
+        if _LUA_SHA_DECREASE is None:
+            _LUA_SHA_DECREASE = redis_client.register_script(_LUA_SCRIPT_DECREASE)
 
-        r = _LUA_SHA(keys=[key], args=[KEY_LIMIT, 60])
-        redis_client.decr(key, 1)
-
-
-def task_id_limiter(func):
-    @wraps(func)
-    def inner(*args, **kwargs):
-        key = args[0]
-        global _LUA_SHA
-        if _LUA_SHA is None:
-            _LUA_SHA = redis_client.register_script(_LUA_SCRIPT)
-
-        check_key = _LUA_SHA(keys=[key], args=[KEY_LIMIT, 60])
-        if check_key:
-            with key_release(key):
-                return func(*args, **kwargs)
-        else:
-            dummy_task.apply_async(args=args, countdown=RETRY_SEC)
-    return inner
+        r = _LUA_SHA_DECREASE(keys=[key], args=[600])
 
 
-@celery.task
-@task_id_limiter
+def bind_arguments(func, *args, **kwds):
+    signature = inspect.signature(func)
+    bound = signature.bind(*args, **kwds)
+    bound.apply_defaults()
+    return bound.arguments.copy()
+
+
+def task_limiter(tkey):
+
+    def key_factory(func, *args, **kwargs):
+        if isinstance(tkey, str):
+            key_params = dict(bind_arguments(func, *args, **kwargs))
+            key_params["task_name"] = func.__name__
+            return tkey.format_map(key_params)
+
+        return tkey(*args, **kwargs)
+
+    def retry(func, *args, **kwargs):
+        globals()[func.__name__].apply_async(args=args, countdown=RETRY_SEC)
+
+    def decor(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            key = key_factory(func, *args, **kwargs)
+            global _LUA_SHA_INCREASE
+            if _LUA_SHA_INCREASE is None:
+                _LUA_SHA_INCREASE = redis_client.register_script(_LUA_SCRIPT_INCREASE)
+            check_key = _LUA_SHA_INCREASE(keys=[key], args=[KEY_LIMIT, 600])
+
+            if check_key <= KEY_LIMIT:
+                with key_releaser(key):
+                    return func(*args, **kwargs)
+            else:
+                retry(func, *args, **kwargs)
+        return inner
+    return decor
+
+
+@celery.task(acks_late=True, reject_on_worker_lost=True)
+@task_limiter(tkey="task_limit:{task_name}:{task_key}")
 def dummy_task(task_key, sec_to_sleep):
     time.sleep(sec_to_sleep)
     return sec_to_sleep
